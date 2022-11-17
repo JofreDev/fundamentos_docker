d045d78cbbb3a7eebdcf18881b4fd2ef3d221abb
JofreDev <gavidiajofreeduardo@gmail.com>
2022-11-17 11:21:46 -0500

    Se añadió Docker compose en modo dev

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 mongoose.connect('mongodb://PapiJofre:password@mongous:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando... chanchitos...ejemploo2 xd')
   const animales = await Animal.find();
   return res.send(animales)
 })
